ed5d307b42b45f9dd6a34103e6f884ed95efe216
piyush85-byte <piyush.patil@antino.com>
2024-11-26 23:16:18 +0530

    added request

--- index.js
+++ index.js
@@ -8,4 +8,9 @@ const app=express()
 
 app.use(cors())
 
+app.get("/",(req,res)=>{
+    res.status(202).json("Server running we are live.")
+})
+
+
 app.listen(5500,()=>console.log("Server started on port 5500"))
