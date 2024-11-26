bff675622f8196cadbc0ec28e390954762842dec
piyush85-byte <piyush.patil@antino.com>
2024-11-26 23:05:47 +0530

    initiated nodejs process

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Jenkins test log 8")
+const express=require("express")
+const cors=require("cors")
+
+
+const app=express()
+
+
+
+app.listen(5500,()=>console.log("Server started on port 5500"))
