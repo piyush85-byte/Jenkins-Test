42e2b4dda55252f7c639055ea9e46bb0f55dfaa7
piyush85-byte <piyush.patil@antino.com>
2024-12-06 16:42:29 +0530

    init 2

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.status(202).json("Server running we are live and connected to jenkins.");
+  res.status(202).json("Server running we are live.");
 });
 
-app.listen(5500, () => console.log("Server started on port 5500"));
+app.listen(5500, () =>
+  console.log("Server started on port 5500. Jenkins log 1")
+);
