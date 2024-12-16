1c527f02894a40e1d3388343c02faf5058269654
piyush85-byte <piyush.patil@antino.com>
2024-12-16 08:08:02 +0530

    fix 2

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const express=require("express")
-const cors=require("cors")
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
 
+app.use(cors());
 
+app.get("/", (req, res) => {
+  res.status(202).json("Server running we are live.");
+});
 
-const app=express()
-
-app.use(cors())
-
-app.get("/",(req,res)=>{
-    res.status(202).json("Server running we are live.")
-})
-
-
-app.listen(5500,()=>console.log("Server started on port 5500"))
+app.listen(5500, () =>
+  console.log("Server started on port 5500  This is log 2 ")
+);
